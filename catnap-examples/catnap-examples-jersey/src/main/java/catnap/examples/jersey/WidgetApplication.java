52c6554d6fea1f478144053c93dae66568741c2f
Whitaker, Greg <Greg.Whitaker@nike.com>
2016-06-02 16:22:55 -0700

    Adding jersey example

--- catnap-examples/catnap-examples-jersey/src/main/java/catnap/examples/jersey/WidgetApplication.java
+++ catnap-examples/catnap-examples-jersey/src/main/java/catnap/examples/jersey/WidgetApplication.java
@@ -1,6 +1,25 @@
 package catnap.examples.jersey;
 
+import catnap.examples.jersey.resource.WidgetResource;
+import com.github.gregwhitaker.catnap.jaxrs.view.JsonCatnapMessageBodyWriter;
+import com.github.gregwhitaker.catnap.jaxrs.view.JsonpCatnapMessageBodyWriter;
+
 import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
 
 public class WidgetApplication extends Application {
+    private final Set<Object> singletons = new HashSet<>();
+
+    public WidgetApplication() {
+        singletons.add(new WidgetResource());
+
+        singletons.add(new JsonCatnapMessageBodyWriter());
+        singletons.add(new JsonpCatnapMessageBodyWriter());
+    }
+
+    @Override
+    public Set<Object> getSingletons() {
+        return singletons;
+    }
 }
--- catnap-examples/catnap-examples-jersey/src/main/java/catnap/examples/jersey/resource/WidgetResource.java
+++ catnap-examples/catnap-examples-jersey/src/main/java/catnap/examples/jersey/resource/WidgetResource.java
@@ -1,4 +1,28 @@
 package catnap.examples.jersey.resource;
 
+import catnap.examples.jersey.model.Widget;
+import catnap.examples.jersey.service.WidgetService;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import java.util.List;
+
+@Path("widgets")
+@Produces({"application/json", "application/x-javascript"})
 public class WidgetResource {
+
+    private WidgetService service = new WidgetService();
+
+    @GET
+    public List<Widget> getWidgets() {
+        return service.getWidgets();
+    }
+
+    @GET
+    @Path("/{id}")
+    public Widget getWidget(@PathParam("id") String id) {
+        return service.getWidget(id);
+    }
 }
